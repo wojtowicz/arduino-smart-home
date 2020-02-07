c922946f7db468c6a691876b5485e61052aa65f3
Tomasz Wójtowicz <tomash2310w@gmail.com>
2020-02-07 14:36:53 +0100

    Remove environment files from repository

--- src/environments/environment.local.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  local: true,
-  dataSource: 'local',
-  message: 'hello local'
-};
--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  local: false,
-  dataSource: 'remote',
-  message: 'hello prod'
-};
